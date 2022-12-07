a520f70dd5c7d836964977cdead498c9c3046146
Lazola2 <lazolamakubalo271@gmail.com>
2022-12-07 16:18:58 +0200

    created a modal

--- js/code.js
+++ js/code.js
@@ -4,70 +4,70 @@ let items = [
         brand: 'Lenovo',
         price: 9000,
         specifications: ['Lenovo ThinkPad', 'Intel Core i5 Processor', '8GB RAM', '500GB HDD', 'Windows 10 Pro'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        imageLink: 'https://i.postimg.cc/Jzd7CVzQ/Lenovo-thinkpad.webp'
     },
     {
         id: 2,
         brand: 'MacBook Pro',
         price: 17500,
         specifications: ['MacBook Pro', 'Intel Core i5 Processor', '8GB RAM', '256GB SSD', 'MacOS'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        imageLink: 'https://i.postimg.cc/d3MLPMnf/Mac-Book-Pro.png'
     }, 
     {
         id: 3,
         brand: 'Dell',
-        price: 12500,
-        specifications: ['Dell Slim pad', 'Intel Core i5 Processor', '4GB RAM', '500GB HDD', 'Windows 11'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        price: 18500,
+        specifications: ['Dell XPS', 'Intel Core i5 Processor', '4GB RAM', '500GB HDD', 'Windows 11'],
+        imageLink: 'https://i.postimg.cc/02K1KMk7/Dell-XPS.jpg'
     }, 
     {
         id: 4,
         brand: 'Meccer',
         price: 21000,
         specifications: ['Meccer FlipPad', '2.8Hz Processor', '8GB RAM', '256GB SSD', 'Windows 10 Pro'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        imageLink: 'https://i.postimg.cc/vHBpF1gp/Mecer-Flip.jpg'
     }, 
     {
         id: 5,
         brand: 'Asus',
         price: 5600,
-        specifications: ['Asus XPro', 'i5 Processor', '4GB Ram', '256GB HDD', 'Windows 10'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        specifications: ['Asus Zenbook Pro', 'i5 Processor', '4GB Ram', '256GB HDD', 'Windows 10'],
+        imageLink: 'https://i.postimg.cc/T2k49R7Q/Asus-Zenbook-pro.jpg'
     }, 
     {
         id: 6,
         brand: 'Acer',
         price: 16000,
         specifications: ['Acer Aspire 3', 'Intel Core i5 Processor', '4GB RAM', '256GB SSD', 'Windows 11'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        imageLink: 'https://i.postimg.cc/3r9BTn2w/Acer-Aspire-3-blue.webp'
     },
     {
         id: 7,
         brand: 'Lenovo',
         price: 9000,
-        specifications: ['Lenovo IdeaPad', 'Intel Core i5 Processor', '8GB RAM', '256GB SSD', 'Windows 10 Pro'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        specifications: ['Lenovo Slim', 'Intel Core i5 Processor', '8GB RAM', '256GB SSD', 'Windows 10 Pro'],
+        imageLink: 'https://i.postimg.cc/Qxdkf19W/Lenovo-Slim.jpg'
     },
     {
         id: 8,
         brand: 'HP',
         price: 7500,
         specifications: ['HP 15 Series', 'Intel Celeron', '$GB RAM', '500GB HDD', 'Windows 10 Pro'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        imageLink: 'https://i.postimg.cc/KvGgZbmX/hp.webp'
     },
     {
         id: 9,
         brand: 'Asus',
         price: 36000,
         specifications: ['Asus ROG G15 Ryzen', 'i5 Processor', '8GB RAM', '1TB HDD', 'Windows 11'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        imageLink: 'https://i.postimg.cc/8csrDBVF/Asus-ROG.png'
     },
     {
         id: 10,
         brand: 'Asus',
         price: 9000,
         specifications: ['Asus Extensa', 'Intel Core i5 Processor', '8GB RAM', '256GB SSD', 'Windows 10 Pro'],
-        imageLink: 'https://i.postimg.cc/qBPXzD3m/download.jpg'
+        imageLink: 'https://i.postimg.cc/SR8YJQJq/Asus-Extensa.jpg'
     }
 ]
 
