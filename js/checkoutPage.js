05b211496552d5cf52a35147f3b263b75c3c00af
Lazola2 <lazolamakubalo271@gmail.com>
2022-12-07 22:13:06 +0200

    indented code structure on checkout.js for clarity

--- js/checkoutPage.js
+++ js/checkoutPage.js
@@ -16,26 +16,24 @@ function removeCheckoutItem(item){
 }
 
 function renderCheckout() {
-      // load the checkout content
-      checkoutData.forEach(checkoutItem => {
-          let item = `
-          <div class="checkout-item rounded-3  overflow-hidden my-3 d-flex align-items-center justify-content-between">
-                          <img src="${checkoutItem.imageLink}" class="ms-3">
-                          <div class="bg-dark content-holder me-1 rounded-2 overflow-hidden d-flex align-items-center justify-content-between">
-                              <div class="content ms-2 me-2 rounded-1 text-white ps-3">
-                                  <p class=" mb-1brand p-0 m-0 fw-bold fs-5 mb-2">${checkoutItem.brand}</p>
-                                  <p class=" mb-1quantity p-0 m-0">Quantity: ${1}</p>
-                                  <p class=" mb-1specifications p-0 m-0">Specification: ${checkoutItem.specifications}</p>
-                                  <p class=" mb-1total p-0 m-0">Total: R-- ---</p>
-
-                              </div>
-                              <button class="btn btn-danger btn-remove me-4" onclick='removeCheckoutItem(${JSON.stringify(checkoutItem)})'>Remove</button>
-                          </div>
-                      </div>`
+    // load the checkout content
+    checkoutData.forEach(checkoutItem => {
+        let item = `
+            <div class="checkout-item rounded-3  overflow-hidden my-3 d-flex align-items-center justify-content-between">
+                <img src="${checkoutItem.imageLink}" class="ms-3">
+                <div class="bg-dark content-holder me-1 rounded-2 overflow-hidden d-flex align-items-center justify-content-between">
+                    <div class="content ms-2 me-2 rounded-1 text-white ps-3">
+                        <p class=" mb-1brand p-0 m-0 fw-bold fs-5 mb-2">${checkoutItem.brand}</p>
+                        <p class=" mb-1quantity p-0 m-0">Quantity: ${1}</p>
+                        <p class=" mb-1specifications p-0 m-0">Specification: ${checkoutItem.specifications}</p>
+                        <p class=" mb-1total p-0 m-0">Total: R-- ---</p>
+                    </div>
+                    <button class="btn btn-danger btn-remove me-4" onclick='removeCheckoutItem(${JSON.stringify(checkoutItem)})'>Remove</button>
+                </div>
+            </div>`
           // console.log(item)
-          checkoutContainer.innerHTML += item;
-      });
-
+        checkoutContainer.innerHTML += item;
+    });
 }
 
 function displayCheckout() {
