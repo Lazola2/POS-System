8302dcfd36c67d273ca17c7ca4b7c9855f37630b
Lazola2 <lazolamakubalo271@gmail.com>
2022-12-09 22:29:13 +0200

    h

--- js/header.js
+++ js/header.js
@@ -1,8 +1,13 @@
 let closeButton = document.querySelector('.close-button');
+let menuButton = document.querySelector('.menu-icon');
+
 let navigation = document.querySelector('.navigation');
 let navigationLinks = document.querySelector('.nav-links');
 
+menuButton.addEventListener('click', () => {
+    navigation.classList.remove('removed');
+})
+
 closeButton.addEventListener('click', () => {
     navigation.classList.add('removed');
-    // navigationLinks.classList.add('removed');
 })
